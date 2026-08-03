0b207de27a857e32b31f20c3e6cf2b0ad8a67b89
Israt-Nisha <isratnisha1999@gmail.com>
2026-08-03 22:06:56 +0600

    feat: start implement auth modules

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,56 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import { AuthResponseDto } from './dto/auth-response.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+    private readonly SALT_ROUNDS = 12;
+    constructor(private prisma: PrismaService) { }
+
+    // Register a new user
+    async register(registerDto: RegisterDto): Promise<AuthResponseDto> {
+
+        const { email, password, firstName, lastName } = registerDto;
+        // Check if user already exists
+        const existingUser = await this.prisma.user.findUnique({
+            where: { email }
+        });
+
+        if (existingUser) {
+            throw new ConflictException('User with this email already exists');
+        }
+
+        try {
+            const hashedPassword = await bcrypt.hash(password, this.SALT_ROUNDS);
+            const user = await this.prisma.user.create({
+                data: {
+                    email,
+                    password: hashedPassword,
+                    firstName,
+                    lastName
+                },
+                select: {
+                    id: true,
+                    email: true,
+                    firstName: true,
+                    lastName: true,
+                    role: true,
+                    password: false
+                }
+            });
+            // const tokens = await this.generateTokens(user.id, user.email, user.role);
+            return {
+                accessToken: 'null',
+                refreshToken: 'null',
+                user
+            };
+        } catch (error) {
+            throw new Error(`Failed to register user: ${error.message}`);
+
+        }
+    }
+
+    // Generate access and refresh tokens
+}
---
+++ src/modules/auth/dto/register.dto.ts
@@ -0,0 +1,23 @@
+// Data transper object (DTO) for user registration
+
+import { IsEmail, IsNotEmpty, IsOptional, IsString, MinLength,} from "class-validator";
+
+export class RegisterDto {
+    
+    @IsEmail({}, { message: 'Please provide a valid email address' })
+    @IsNotEmpty({ message: 'Email is required' })
+    email: string;
+
+    @IsString()
+    @IsNotEmpty({ message: 'Password is required' })
+    @MinLength(8, { message: 'Password must be at least 8 characters long' })
+    password: string;
+
+    @IsString()
+    @IsOptional()
+    firstName?: string;
+
+    @IsString()
+    @IsOptional()
+    lastName?: string;
+}
